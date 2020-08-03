ca27d9ff0ba8dbf13111f0a8fc983d9c5fd37bb6
joe waine <joe.waine@gmail.com>
2020-08-03 11:35:14 -0700

    index

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const qs = require('qs');
 const mongoose = require("mongoose");
 const config = require("./config/db");
 const app = express();
-// app.use(cors());
+app.use(cors({
+  origin: 'https://www.nadimama.com'
+}));
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+
+
 
 
 //configure database and mongoose
