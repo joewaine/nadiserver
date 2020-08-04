75ebb910e26702e70e8f8faa7c1a8eb0113a26b9
joe waine <joe.waine@gmail.com>
2020-08-04 14:26:46 -0700

    config

--- index.js
+++ index.js
@@ -22,7 +22,24 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // app.use(cors());
 
 
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.header('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 
 
@@ -83,21 +100,3 @@ app.listen(PORT, () => {
 
 
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
