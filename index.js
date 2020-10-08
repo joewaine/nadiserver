82405e0e132a875f0af065e8225923b6fbb928fc
joe waine <joe.waine@gmail.com>
2020-10-08 15:45:16 -0700

    update

--- index.js
+++ index.js
@@ -142,11 +142,11 @@ let axiosConfig = {
   }
 };
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 
