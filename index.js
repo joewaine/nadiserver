b5da49b85ac8f6fdbd79f7f74091973c24d6b8b1
joe waine <joe.waine@gmail.com>
2020-10-09 12:56:40 -0700

    edit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.options('*', cors());
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nadimama.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
