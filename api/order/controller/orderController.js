1c204839f7688ef9f6ee9e3fa31fc780819dd924
joe waine <joe.waine@gmail.com>
2020-10-15 15:25:37 -0700

    d

--- api/order/controller/orderController.js
+++ api/order/controller/orderController.js
@@ -7,21 +7,14 @@ const e = require("express");
 
 
 exports.addOrder = async (req, res) => {
-    console.log('add to mongo emerge pay back end')
-    // console.log(JSON.stringify(req.body))
-    console.log(req.headers)
-    req.header("Access-Control-Allow-Origin", "*");
     try {
         const order = new Order({
           email: req.body.payInfo.fulfillment_info.customer.email,
           payInfo: req.body.payInfo,
           orderInfo: req.body.orderInfo
         });
-        console.log(order)
+        // console.log(order)
         let data = await order.save();
-        // res.status(201).json({ data });
-        // res.send(201)
-        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.json({ data });
 
 
@@ -54,26 +47,3 @@ console.log(req.params.email)
 
   }
  };
-
-
-  
-  
-  
-  // try {
-  //   const request = await fetch('https://hq.breadcrumb.com/ws/v1/menus/online_ordering/', {
-  //     headers: {
-  //       'X-Breadcrumb-Username': `generic-online-ordering_mamnoon-llc`,
-  //       'X-Breadcrumb-Password': 'uQM8mseTvnTX',
-  //       'X-Breadcrumb-API-Key': `e2ebc4d1af04b3e5e213085be842acaa`  
-  //     }
-  //   })
-  //   if (request.ok) { 
-  //     const body = await request.json();
-  //     // console.log(body)
-  //     res.status(201).json({ body });
-  //   }
-  // } catch (err) {
-  //  res.status(400).json({ err: err });
-  // }
-
-
--- index.js
+++ index.js
@@ -48,11 +48,6 @@ mongoose
     console.log({ database_error: err });
   });
 
-
-// console.log(cors)
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -70,8 +65,6 @@ app.use("/user", userRoutes);
 const productRoutes = require("./api/product/route/product"); //bring in our product routes
 app.use("/product", productRoutes);
 
-
-
 const orderRoutes = require("./api/order/route/order"); //bring in our product routes
 app.use("/order", orderRoutes);
 
@@ -80,48 +73,26 @@ const e = require("express");
 
 app.use("/tock", tockRoutes);
 
-
-
-//Ensure that you replace these with valid values before trying to issue a request
 var oid = "1517492274";
 var authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aWQiOjMwNywib2lkIjoxNTE3NDkyMjc0LCJ0b2tlbl91c2UiOiJvcnQiLCJybmQiOjEyOTgyMzk1ODYuMDY0MjgyNCwiZ3JvdXBzIjpbIk9yZ0FQSVVzZXJzIl0sImlhdCI6MTU5OTI1ODg3MH0.zaMi_DDPspTKW6fl2utCGKXwdQT-Q39DKrFOhXxCHA4";
 var environmentUrl = "https://api.emergepay-sandbox.chargeitpro.com/virtualterminal/v1";
-// var environmentUrl = "https://nadimama.com/mamnoon";
 var emergepay = new sdk.emergepaySdk({ oid: oid, authToken: authToken, environmentUrl: environmentUrl });
-
-
-// orgs/1517492274/transactions/start
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-
-
-
-
-
-//add order
-
-console.log(Order)
-
-
-//add order
 
 //The client side can hit this endpoint by issuing a POST request to
-//localhost:5555/start-transaction
 //base_amount and external_tran_id are required in the fields array.
 app.post("/start-transaction", function (req, res) {
 let amount = Number(req.body.charges.preTotal)
 let tipAmount = Number(req.body.charges.tip.amount)
 let formattedTipAmount = tipAmount/100
 
-console.log(req.body.charges.tip)
+
 let finalAmount = amount
 let finalCash = finalAmount/100
 let config = {
     transactionType: sdk.TransactionType.CreditSale,
-    // transactionType: sdk.TransactionType.CreditAuth,
-    // method: "hostedFields",
     method: "modal",
     fields: [
         {
@@ -153,8 +124,6 @@ let config = {
   
     emergepay.startTransaction(config)
     .then(function (transactionToken) {
-      // console.log('success')
-      // console.log(transactionToken)
         res.send({
             transactionToken: transactionToken
         });
@@ -165,54 +134,10 @@ let config = {
     });
 });
 
-// console.log(PORT);
-
-// retrieval method
-// emergepay.retrieveTransaction("d492296a-2ecd-4c64-8768-b186869257f7")
-// .then(function(response) {
-//     var transactionResponse = response.data;
-//     console.log(transactionResponse)
-// })
-// .catch(function(error) {
-//     throw error;
-// });
-
-// retrieval method
-
-
-// emerge pay stuff
-// emerge pay stuff
-// emerge pay stuff
-
 app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
 });
 
-
-let postOnlineOrder = async (req, res) => {
-  
-  
-
-
-  
-console.log(345)
-  
-  axios.post('https://hq.breadcrumb.com/ws/v1/orders', req,
-  {
-  headers: {
-    'X-Breadcrumb-Username': `generic-online-ordering_mamnoon-llc`,
-    'X-Breadcrumb-Password': 'uQM8mseTvnTX',
-    'X-Breadcrumb-API-Key': `e2ebc4d1af04b3e5e213085be842acaa`
-
-}})
-     .then(function (res) {
-        console.log(res.data)
-     })
-     .catch(function (error) {
-       console.log(error)
-     });
-      }
-       
 
        var transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -235,35 +160,20 @@ console.log(345)
                   let resData = response.data
                   console.log(response)
                   if(resData.result === 'success'){
-
-
-                    // xxx
                     res.send(req.body)
-
-
                       let htmlBody = '<ul>'
-
                       for(let i = 0;i<req.body.charges.items.length;i++){
-
                         htmlBody = htmlBody + '<li>' + JSON.stringify(req.body.charges.items[i].name) + '&nbsp;<b>$'+ JSON.stringify(req.body.charges.items[i].price) +'</b>&nbsp;x&nbsp;'+ JSON.stringify(req.body.charges.items[i].quantity) +'</li>'
                         
-
                       }
                       
-                      htmlBody = htmlBody + '</ul>'
-
-
-
-                      // "name":"961 Lebanese Pale Ale","price":800,quantity":1
-
+                    htmlBody = htmlBody + '</ul>'
 
                     var mailOptions = {
                       from: 'joe@mamnoonrestaurant.com',
                       to: req.body.fulfillment_info.customer.email,
-                      // to: 'wassef@mamnoonrestaurant.com, sofien@mamnoonrestaurant.com, joe.waine@gmail.com',
                       subject: 'Your Mamnoon Order Has Been Received!',
                       html: '<pre>'+JSON.stringify(req.body.charges.items)+'</pre>'
-                      
                     };
                     
                     transporter.sendMail(mailOptions, function(error, info){
@@ -273,51 +183,13 @@ console.log(345)
                         console.log('Email sent: ' + info.response);
                       }
                     });
-// xxx
-                  }else{
-                    res.send('result not success')
-
-                    res.send(req.body)
-
-
-                      let htmlBody = '<ul>'
-
-                      for(let i = 0;i<req.body.charges.items.length;i++){
-
-                        htmlBody = htmlBody + '<li>' + JSON.stringify(req.body.charges.items[i].name) + '&nbsp;<b>$'+ JSON.stringify(req.body.charges.items[i].price).toFixed(2)/100 +'</b>&nbsp;x&nbsp;'+ JSON.stringify(req.body.charges.items[i].quantity) +'</li>'
-                        
-
-                      }
-                      
-                      htmlBody = htmlBody + '</ul>'
-
-console.log(htmlBody)
-
-                      // "name":"961 Lebanese Pale Ale","price":800,quantity":1
-
-
-                    var mailOptions = {
-                      from: 'joe@mamnoonrestaurant.com',
-                      to: req.body.fulfillment_info.customer.email,
-                      // to: 'wassef@mamnoonrestaurant.com, sofien@mamnoonrestaurant.com, joe.waine@gmail.com',
-                      subject: 'Your Mamnoon Order Has Been Received!',
-                      html: '<pre>'+JSON.stringify(req.body)+'</pre>'
-                    };
-                    
-                    transporter.sendMail(mailOptions, function(error, info){
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        console.log('Email sent: ' + info.response);
-                      }
-                    });
-
-
-
                   }
                })
                  .catch(function (error) {
                    console.log(error)
+
+
+
                  });
               
       });
